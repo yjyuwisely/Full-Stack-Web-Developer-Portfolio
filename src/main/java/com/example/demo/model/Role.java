7df15abb522cf41427802173ed7bcfd0811a281d
yjyuwisely <yjyuwisely@gmail.com>
2023-04-16 18:33:15 +0900

    I changed some styles, saving this before deleting lastname column.

--- src/main/java/com/example/demo/model/Role.java
+++ src/main/java/com/example/demo/model/Role.java
@@ -1,42 +1,30 @@
-package com.example.demo.model;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-
-@Entity
-@Table(name = "role")
-public class Role {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-
-	public Role() {
-
-	}
-
-	public Role(String name) {
-		super();
-		this.name = name;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-}
+/*//I don't use this at the moment (230416) since I did not divide users into users and admin.
+ * package com.example.demo.model;
+ * 
+ * import javax.persistence.Entity; import javax.persistence.GeneratedValue;
+ * import javax.persistence.GenerationType; import javax.persistence.Id; import
+ * javax.persistence.Table;
+ * 
+ * @Entity
+ * 
+ * @Table(name = "role") public class Role {
+ * 
+ * @Id
+ * 
+ * @GeneratedValue(strategy = GenerationType.IDENTITY) private Long id; private
+ * String name;
+ * 
+ * public Role() {
+ * 
+ * }
+ * 
+ * public Role(String name) { super(); this.name = name; }
+ * 
+ * public Long getId() { return id; }
+ * 
+ * public void setId(Long id) { this.id = id; }
+ * 
+ * public String getName() { return name; }
+ * 
+ * public void setName(String name) { this.name = name; } }
+ */
